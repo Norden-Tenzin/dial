519fe19450ff1d4a4f17e3ff6d89ddbde4104b4f
Norden-Tenzin <nordentenzin29@gmail.com>
2024-10-05 21:52:14 +0530

    changes to packages file

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "dial",
+    name: "Dial",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "dial",
-            targets: ["dial"]
+            name: "Dial",
+            targets: ["Dial"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "dial"
+            name: "Dial"
         ),
         .testTarget(
-            name: "dialTests",
-            dependencies: ["dial"]
+            name: "DialTests",
+            dependencies: ["Dial"]
         )
     ]
 )
